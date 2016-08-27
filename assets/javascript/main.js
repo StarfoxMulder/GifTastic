60c48a2e301479173206a09c3c386c55346c9fb9
Charles Prestia <Charles Prestia>
2016-08-27 15:53:47 -0400

    updating

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
-	$('button').on('click', function() {
+	$('.btn-secondary').click(function() {
+		console.log("anything?");
         var cryptid = $(this).data('cryptid');
+        console.log(cryptid);
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cryptid + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         $.ajax({
@@ -10,52 +12,40 @@ $(document).ready(function() {
             })
             .done(function(response) {
                 console.log(queryURL);
-                console.log(response)
+                console.log(response);
 
                 var results = response.data;
 
                 for (var i = 0; i < results.length; i++) {
 
-                    /* step 3: 
-                        * uncomment the for loop above and the closing curly bracket below
-                        * make a div and reference it in a variable named animalDiv
-                        * make a paragraph tag and put it in a variable named p
-                            * set the text of the paragraph to the rating of the image in results[i]
-                        * make an image and reference it in a variable named animalImage
-                        * set the image's src to results[i]'s fixed_height.url 
-
-                        * append the p variable to the animalDiv variable
-                        * append the animalImage variable to the animalDiv variable
-
-                        * prepend the animalDiv variable to the element with an id of gifsAppearHere
-
-                    */
-
-                    //------------put step 3 in between these dashes--------------------
                     var cryptidDiv = $('<div>');
 
-                    var p = $('<p>').text("Rating: " + results[i].rating);
+                    var h2 = $('<h2>').text("Rating: " + results[i].rating);
 
                     var cryptidImage = $('<img>');
                     cryptidImage.attr('src', results[i].images.fixed_height.url);
 
-                    cryptidDiv.append(p);
+                    cryptidDiv.append(h2);
                     cryptidDiv.append(cryptidImage);
 
                     $('#gifContainer').append(cryptidDiv);
+
+                    topics.push(cryptid);
                 }
 
-            });
+            })
     });
 
     var topics = [
-    	sashquatch,
-    	yeti,
-    	chupacabra,
-    	greys,
-    	mothman,
+    	'sashquatch',
+    	'yeti',
+    	'chupacabra',
+    	'greys',
+    	'mothman',
     ];
 
     for(var i =0; i < topics.length; i++) {
-
-    }
+    	var but = "<button type='button' class='btn btn-secondary' data-cryptid='"+topics[i]+"'>"+topics[i]+"</button>";
+    	$('#gifButtons').append(but);
+    };
+});
