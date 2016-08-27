81baeee748b0ebc081c1528bb185ad4d4c902eaf
Charles Prestia <Charles Prestia>
2016-08-26 22:25:32 -0400

    pushing initial files

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	
+	var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+	$.ajax({url: queryURL, method: 'GET'})
+	 .done(function(response) {
+	     console.log(response);
+	}); 
+
